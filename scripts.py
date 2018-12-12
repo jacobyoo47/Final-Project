e4d158050296dca7ee4225e1e4b6656c1f55ce75
godpng <jakey47@gmail.com>
2018-12-12 14:44:04 -0800

    added up and down sprites for character

--- scripts.py
+++
@@ -1,9 +0,0 @@
-import math
-import arcade
-import os
-
-def displayTextBox(text, x, y, width, height, color):
-
-    arcade.draw_rectangle_filled(x, y, width, height, color)
-    
-    arcade.draw_text(text, 20, height - 35, arcade.color.BLACK, 16)
