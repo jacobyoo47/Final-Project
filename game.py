e98081700fb64eca814e89fa2d3b58cc822cbfe3
Jason-Chen-HMC <jaschen@g.hmc.edu>
2018-12-13 17:38:08 -0800

    Added GUI

--- game.py
+++ game.py
@@ -23,6 +23,7 @@ SCREEN_WIDTH = SPRITE_SIZE * 30
 SCREEN_HEIGHT = SPRITE_SIZE * 16
 
 #Game states
+START = -1
 GAME = 0
 DIALOGUE = 1
 INVENTORY = 2
@@ -239,6 +240,118 @@ def setup_room_2():
     return room
 
 
+
+class TextButton:
+    """ Text-based button """
+    def __init__(self,
+                 center_x, center_y,
+                 width, height,
+                 text,
+                 font_size=18,
+                 font_face="Arial",
+                 face_color=arcade.color.LIGHT_GRAY,
+                 highlight_color=arcade.color.WHITE,
+                 shadow_color=arcade.color.GRAY,
+                 button_height=2):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.font_size = font_size
+        self.font_face = font_face
+        self.pressed = False
+        self.face_color = face_color
+        self.highlight_color = highlight_color
+        self.shadow_color = shadow_color
+        self.button_height = button_height
+
+    def draw(self):
+        """ Draw the button """
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width,
+                                     self.height, self.face_color)
+
+        if not self.pressed:
+            color = self.shadow_color
+        else:
+            color = self.highlight_color
+
+        # Bottom horizontal
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y - self.height / 2,
+                         self.center_x + self.width / 2, self.center_y - self.height / 2,
+                         color, self.button_height)
+
+        # Right vertical
+        arcade.draw_line(self.center_x + self.width / 2, self.center_y - self.height / 2,
+                         self.center_x + self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        if not self.pressed:
+            color = self.highlight_color
+        else:
+            color = self.shadow_color
+
+        # Top horizontal
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y + self.height / 2,
+                         self.center_x + self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        # Left vertical
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y - self.height / 2,
+                         self.center_x - self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        x = self.center_x
+        y = self.center_y
+        if not self.pressed:
+            x -= self.button_height
+            y += self.button_height
+
+        arcade.draw_text(self.text, x, y,
+                         arcade.color.BLACK, font_size=self.font_size,
+                         width=self.width, align="center",
+                         anchor_x="center", anchor_y="center")
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        self.pressed = False
+
+
+def check_mouse_press_for_buttons(x, y, button_list):
+    """ Given an x, y, see if we need to register any button clicks. """
+    for button in button_list:
+        if x > button.center_x + button.width / 2:
+            continue
+        if x < button.center_x - button.width / 2:
+            continue
+        if y > button.center_y + button.height / 2:
+            continue
+        if y < button.center_y - button.height / 2:
+            continue
+        button.on_press()
+
+
+def check_mouse_release_for_buttons(x, y, button_list):
+    """ If a mouse button has been released, see if we need to process
+        any release events. """
+    for button in button_list:
+        if button.pressed:
+            button.on_release()
+
+
+class StartTextButton(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "Start", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+
 class MyGame(arcade.Window):
     """ Main application class. """
     
@@ -254,6 +367,9 @@ class MyGame(arcade.Window):
         # as mentioned at the top of this program.
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
+
+        # Button list
+        self.button_list = None
 
         # Sprite lists
         self.current_room = 0
@@ -284,7 +400,13 @@ class MyGame(arcade.Window):
         self.player_list.append(self.player_sprite)
 
         #Setting the state of the game
-        self.state = GAME
+        self.state = START
+
+        # Our button list
+        self.button_list = []
+        
+        start_button = StartTextButton(60, 570, self.start_game)
+        self.button_list.append(start_button)
 
         # Our list of rooms
         self.rooms = []
@@ -301,6 +423,19 @@ class MyGame(arcade.Window):
 
         # Create a physics engine for this room
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.rooms[self.current_room].wall_list)
+
+    def start_game(self):
+        """
+        Start the game
+        """
+        self.state = GAME
+
+    def draw_start(self):
+        """
+        Draw the start menu
+        """
+        for button in self.button_list:
+            button.draw()
 
     def draw_game(self):
         """
@@ -346,7 +481,10 @@ class MyGame(arcade.Window):
         """Draws the things on the screen"""
         arcade.start_render()
 
-        if self.state == GAME:
+        if self.state == START:
+            self.draw_start()
+
+        elif self.state == GAME:
             self.draw_game()
 
         elif self.state == DIALOGUE:
@@ -471,6 +609,17 @@ class MyGame(arcade.Window):
             elif key == arcade.key.Z:
                 self.player_sprite.useObject = False
 
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        check_mouse_press_for_buttons(x, y, self.button_list)
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        check_mouse_release_for_buttons(x, y, self.button_list)
 
     def update(self, delta_time):
         """ Movement and game logic """
